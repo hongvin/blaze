5c0655ca1f97b048d587d2737fa8b4967089f35c
Koay Hong Vin <32212455+hongvin@users.noreply.github.com>
2022-01-23 19:59:28 +0800

    Update Footer.js

--- client/src/routes/Pages/components/Footer/Footer.js
+++ client/src/routes/Pages/components/Footer/Footer.js
@@ -4,27 +4,15 @@ import './Footer.scss';
 
 const Footer = () => (
   <footer>
-    <div>
-      <a href="https://bit.ly/36uX8oU" aria-label="DigitalOcean" target="_blank" rel="noreferrer">
-        <img class="digital-ocean" src="https://opensource.nyc3.cdn.digitaloceanspaces.com/attribution/assets/PoweredByDO/DO_Powered_by_Badge_white.svg" alt="Powered by DigitalOcean" />
-      </a>
-    </div>
 
     <div>
       <span>
-        © 2018 - present&nbsp;
-        <a href="https://akashhamirwasia.com" target="_blank" rel="noreferrer">
-          Akash Hamirwasia
-        </a>
-        .&nbsp;
+        hongvin
       </span>
       <span>
-        Released under the
-        <a href="https://github.com/blenderskool/blaze/blob/master/LICENSE" target="_blank" rel="noreferrer"> MIT License</a>
+        Deployed from
+        <a href="https://github.com/blenderskool/blaze/" target="_blank" rel="noreferrer"> Blaze</a>
       </span>
-      <div>
-        Designed & Developed in 🇮🇳
-      </div>
     </div>
   </footer>
 );
