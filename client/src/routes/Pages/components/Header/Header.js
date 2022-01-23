fb00db4f94b9a407f98ea3bc944412f00d5859e3
Koay Hong Vin <32212455+hongvin@users.noreply.github.com>
2022-01-23 20:00:24 +0800

    Update Header.js

--- client/src/routes/Pages/components/Header/Header.js
+++ client/src/routes/Pages/components/Header/Header.js
@@ -31,31 +31,7 @@ function Header() {
         </svg>
         <Pill>v{pkg.version}</Pill>
       </a>
-      
-      <button class="thin icon mobile-menu" onClick={toggleMenu} aria-label="Toggle Menu">
-         {isMenuOpen ? <X size={28} /> : <Menu size={28} />}
-      </button>
-      <nav style={{ display: isMenuOpen ? 'flex' : 'none' }}>
-        <Link activeClassName="active" href="/how-it-works">How it works</Link>
-        <a href="https://github.com/blenderskool/blaze">
-          GitHub&nbsp;
-          <Github size={16} style="margin-bottom: 4px" />
-        </a>
-        <a href="https://www.buymeacoffee.com/akashhamirwasia">Donate</a>
-        <a
-          class="ph-desktop"
-          href="https://www.producthunt.com/posts/blaze-2?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-blaze-2"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=174403&theme=dark&period=daily"
-            alt="Blaze - Fast peer to peer file sharing web app ⚡ | Product Hunt Embed"
-            style="width: 250px; height: 54px;"
-            width="250px"
-            height="54px"
-          />
-        </a>
+
       </nav>
     </header>
   );
